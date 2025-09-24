01f416b4895c5d8e66272279a95ff68c099565d7
J_Canosa <j.canosa@alier.com>
2025-09-24 13:51:50 +0200

    Get_Get_id

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using ContosoPizza.Models;
 using ContosoPizza.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,23 @@ public class PizzaController : ControllerBase
 {
     public PizzaController()
     {
-        //Get all action
-        //Get by Id action
-        //POST action
-        //PUT action
-        //DELETE action
     }
+
+    //Get all action
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+    //Get by Id action
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+        if (pizza == null) return NotFound();
+        return pizza;
+    }
+
+    //POST action
+    //PUT action
+    //DELETE action
+
 }
